32a4b2a432cce41b6ff861355d367fc41fee1a9d
Patryk 'MrDeun' Michalak <pmichalak16@gmail.com>
2024-12-21 17:28:28 +0100

    First commit

---
+++ build/CMakeFiles/C-Raycaster.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "C-Raycaster"
+  "C-Raycaster.pdb"
+  "CMakeFiles/C-Raycaster.dir/src/main.c.o"
+  "CMakeFiles/C-Raycaster.dir/src/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C-Raycaster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/C-Raycaster.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for C-Raycaster.
+# This may be replaced when dependencies are built.
